e47798054252050c2556f8ee961c150f653c19c7
AbhishekDS <abhisheksadale96@gmail.com>
2024-09-09 19:31:49 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Medical ChatBot",
+    version = "0.0.1",
+    author = "Abhishek Sadale (AbhiDS)",
+    author_email = "adsadale@siberindia.edu.in",
+    packages = find_packages(),
+    install_requires = []
+)
