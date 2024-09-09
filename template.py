26fdd4020170867bf41bc21afe0ee1587b59c0e2
AbhishekDS <abhisheksadale96@gmail.com>
2024-09-09 19:04:22 +0530

    folder structure is add

--- template.py
+++ template.py
@@ -14,10 +14,23 @@ list_of_files = [
     "research/trials.ipynb",
     "app.py",
     "store_index.py",
-    "static",
-    "templates/chat.html"
+    "static/gitkeep",
+    "templates/chat.html",
+    
 ]
 
 
 for filepath in list_of_files:
-    path(filepath)
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok = True)
+        logging.info(f"Creating directory;{filedir} for the file {filename}")
+
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"{filename} is already crated")
